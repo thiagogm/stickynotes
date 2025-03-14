2240e826a5bf3472f270ac66ca410d74bdeba179
Thiago Moura <thiagoguimamoura@hotmail.com>
2025-03-13 22:11:06 -0300

    progresso electron

--- main.js
+++ main.js
@@ -116,6 +116,10 @@ const template = [
         type: 'separator'
       },
       {
+        label: 'Recarregar',
+        role: 'reload'
+      },
+      {
         label: 'DevTools',
         role: 'toggleDevTools'
       },
---
+++ src/views/renderer.js
@@ -0,0 +1,13 @@
+// inserção da data no rodapé
+function obterData() {
+    const data = new Date()
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return data.toLocaleDateString('pt-BR', options)
+}
+
+document.getElementById('dataAtual').innerHTML = obterData()
